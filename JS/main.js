9d599bb9a3cdb8a3fdb2d7797e54066a3f26947f
Parzival1608vonKatze <parzival1608@outlook.com>
2021-04-20 15:02:59 +0200

    lol
    lol

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,22 @@
+
+
 let printInConsole ='🛑Felix Stinkt!🛑';
 console.clear()
 console.log(printInConsole);
+
+// js console thx Rico Kahler (https://stackoverflow.com/a/40091811)
+const consoleElm = document.querySelector('#console');
+const clearButton = document.querySelector('#clear');
+
+clearButton.addEventListener('click', (event) => {
+	consoleElm.innerHTML = '';
+});
+
+const consoleForm = document.querySelector('#console-form');
+consoleForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+  const command = event.target.querySelector('#command').value;
+  const value = eval(command);
+  consoleElm.innerHTML += (value === undefined ? 'undefined' : value) + '\n';
+});
+
