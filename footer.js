9c4742cd38c15851997e8c83e85b79d4fa6ee926
Parzival1608vonKatze <parzival1608@outlook.com>
2021-05-29 23:20:14 +0200

    ....

---
+++ footer.js
@@ -0,0 +1,24 @@
+// If you are adding new lines, make sure to escape the line at the end with a \
+
+document.write('\
+    <footer>\
+        <div class="container">\
+            <div class="row">\
+                <div class="col-md-12">\
+                    <div class="social-icons wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="0.3s">\
+                        <ul>\
+                            <li class="twitter"><a target="_blank" href="https://twitter.com/XLabsProject"><i class="fab fa-twitter"></i></a></li>\
+                            <li class="discord"><a target="_blank" href="https://discord.gg/sKeVmR3"><i class="fab fa-discord"></i></a></li>\
+                            <li class="youtube"><a target="_blank" href="https://www.youtube.com/channel/UCPA9X79ZsCXMBBCSJ63wA3A?sub_confirmation=1"><i class="fab fa-youtube"></i></a></li>\
+                            <li class="github"><a target="_blank" href="https://github.com/XLabsProject"><i class="fab fa-github"></i></a></li>\
+                            <li class="patreon"><a target="_blank" href="https://www.patreon.com/xlabsproject"><i class="fab fa-patreon"></i></a></li>\
+                        </ul>\
+                    </div>\
+                    <div class="site-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="0.3s">\
+                        <p>All copyrights reserved &copy; 2021 - <a rel="nofollow" href="https://xlabs.dev">X Labs</a></p>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </footer>\
+');
